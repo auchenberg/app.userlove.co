ee8a9a33b9452a5427e902b4c37b00937b3cfb72
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-01-14 21:36:53 +0100

    Use redis to store sessions.

--- config/session.js
+++ config/session.js
@@ -33,14 +33,14 @@ module.exports.session = {
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
   // Read more about options at: https://github.com/visionmedia/connect-redis
-  //
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
-  // prefix: 'sess:'
 
+  adapter: 'redis',
+  host: 'pub-redis-19015.eu-west-1-2.1.ec2.garantiadata.com',
+  port: 19015,
+  ttl: 0,
+  db: 'rediscloud',
+  pass: '0cAqGWv7Y9gsPWqM',
+  prefix: 'sess:'
 
   // Uncomment the following lines to use your Mongo adapter as a session store
   // adapter: 'mongo',
